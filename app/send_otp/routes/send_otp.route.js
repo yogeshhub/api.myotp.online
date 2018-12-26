55a2173155b3a18760ade62b3bd7ff5fe06296e7
Yogesh KS <yogimayi@outlook.com>
2018-12-26 16:58:08 +0530

    ro

--- app/send_otp/routes/send_otp.route.js
+++ app/send_otp/routes/send_otp.route.js
@@ -6,8 +6,15 @@ const SendOtpController = require('../controllers/send_otp.controller');
 //         SendOtpController.insert
 //     ]);
 // }
+
 const express = require('express');
 var router = express.Router();
+
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+    console.log('Time: ', Date.now())
+    next()
+})
 
 router.post('/generateDigit', [        
     SendOtpController.insert
